9072c118f3f3c4905fef1fd69228746d9871f832
Amit <amitmakkad18@gmail.com>
2022-03-07 21:21:51 +0530

    added instructor navbar

--- frontend/src/components/instructor/Navbar/Navbar.js
+++ frontend/src/components/instructor/Navbar/Navbar.js
@@ -1,13 +1,33 @@
 import React from 'react';
 
 function Navbar() {
-  return (
-    <>
+  return(
+    <> 
       <div>
-        this is the instructor navbar
+        <nav  class="navbar navbar-expand-lg navbar-dark bg-primary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Home</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Profile</a>
+        </li>
+        
+        
+        
+      </ul>
+      
+    </div>
+  </div>
+</nav>
       </div>
-    </>
+      
+       </>
   )
 }
 
 export default Navbar;
+
