5287770f390a6ec0caa0c89d22a52c09c81a271a
Mayank <tayalmayank2000@gmail.com>
2022-03-11 11:28:43 +0530

    added random data

---
+++ backend/Random data/CourseList_record.js
@@ -0,0 +1,27 @@
+let CourseList = ([
+    {
+        courseCode : 'CS 202',
+        courseName : 'Digital Automata',
+        description: 'This is the logic building course which describes about the working of the computer at atomic level.'
+    },
+    {
+        courseCode : 'CS 203',
+        courseName : 'Data Structures and Algorithms',
+        description: 'The most important course for a programming entuatist giving a basic idea of the various algorithms used in competitive programming.'
+    },
+    {
+        courseCode : 'CS 253',
+        courseName : 'Data Structures and Algorithms Lab',
+        description: 'This Course is designed to provide an introduction to the theory and practice of different data structures. This course will also provide familiarity with the algorithms for those data structure.'
+    },
+    {
+        courseCode : 'CS 208',
+        courseName : 'Software Engineering Laboratory',
+        description: 'An highly advanced course to provide students with an environment in which to experience the process of Software Development by working through ‘real world’ projects.'
+    },
+    {
+        courseCode : 'CS 206',
+        courseName : 'Logic Design',
+        description: 'The moto of the course if to provide Knowledge of Basic Electronics and Electrical Engineering.'
+    }
+])
---
+++ backend/Random data/Instructors_record.js
@@ -0,0 +1,69 @@
+let Instructors = ([
+    {
+        instructorEmail : 'cse200001003@iiti.ac.in',
+        generalDetails : {
+            name : 'Amit Anand',
+            dateOfBirth : new Date("1957-02-11"),
+            phoneNumber : '3487934754'
+        },
+    
+        registeredCourses: [
+            {
+                courseCode : 'CS 202',
+                year : '2020',   
+                semester : 'Autumn'
+            },
+            {
+                courseCode : 'CS 202',
+                year : '2021',   
+                semester : 'Autumn'
+            },
+            {
+                courseCode : 'CS 202',
+                year : '2019',   
+                semester : 'Autumn'
+            },
+            {
+                courseCode : 'CS 206',
+                year : '2021',   
+                semester : 'Autumn'
+            },
+            {
+                courseCode : 'CS 206',
+                year : '2020',   
+                semester : 'Autumn'
+            }
+        ]
+    },
+    {
+        instructorEmail : 'cse200001018@iiti.ac.in',
+        generalDetails : {
+            name : 'Contractor Bhavya Thakur',
+            dateOfBirth : new Date("0001-08-27"),
+            phoneNumber : '7654356786'
+        },
+    
+        registeredCourses: [
+            {
+                courseCode : 'CS 203',
+                year : '2021',   
+                semester : 'Spring'
+            },
+            {
+                courseCode : 'CS 208',
+                year : '2021',   
+                semester : 'Spring'
+            },
+            {
+                courseCode : 'CS 253',
+                year : '2022',   
+                semester : 'Spring'
+            },
+            {
+                courseCode : 'CS 208',
+                year : '2021',   
+                semester : 'Autumn'
+            }
+        ]
+    }
+])
