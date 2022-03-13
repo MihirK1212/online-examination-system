5f431163a11034f8f5b414c9b86d715da6a424ac
Amit <amitmakkad18@gmail.com>
2022-03-13 22:09:16 +0530

    Some changes Instructor router

--- frontend/src/pages/instructor/router/Instructor.js
+++ frontend/src/pages/instructor/router/Instructor.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {Routes,Route} from "react-router-dom";
 
 import InstructorHomepage from '../homepage/InstructorHomepage';
+import CourseHomepage from '../homepage/CourseHomepage';
 
 function Instructor() {
   return (
@@ -9,6 +10,8 @@ function Instructor() {
     <div>
         <Routes>
             <Route path="/" element={<InstructorHomepage/>}/>
+            <Route path="/CourseHomepage/:index" element={<CourseHomepage/>}/>
+            
         </Routes>
     </div>
   </>)
