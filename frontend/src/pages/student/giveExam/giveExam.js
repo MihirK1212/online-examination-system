f3bc3cc7e3b3ea02eacae81b517ce1b6a535ad79
Mihir Karandikar <mihirkkarandikar@gmail.com>
2022-03-14 16:08:28 +0530

    Responses tab

--- frontend/src/pages/student/giveExam/giveExam.js
+++ frontend/src/pages/student/giveExam/giveExam.js
@@ -1,10 +1,14 @@
-import { Button } from '@material-ui/core'
+import { Button , Grid } from '@material-ui/core'
 import React, { useState } from 'react'
 import ExamQuestion from '../../../components/student/ExamQuestion/ExamQuestion'
+
+import "./style.css"
 
 function GiveExam() {
 
     let [chosenQnInex,setChosenQnIndex] = useState(0)
+
+    const questionNumbers = [1,2,3,4,5,6,7,8,9,10,11,12,13]
 
     const Questions  = [
         {
@@ -52,15 +56,40 @@ function GiveExam() {
     return (
 
         <>
-            <ExamQuestion question={Questions[chosenQnInex]}/>
+            <div className='examDisplay'>
+                <div className='currentQuestion'>
+                    <ExamQuestion question={Questions[chosenQnInex]}/>
 
-            {
-                chosenQnInex>0?<Button onClick={()=>{setChosenQnIndex(chosenQnInex-1)}}>Previous</Button>:null
-            }
-            {
-                chosenQnInex<(Questions.length-1)?<Button onClick={()=>{setChosenQnIndex(chosenQnInex+1)}}>Next</Button>:null
-            }
+                    <div>
+                        {
+                            chosenQnInex>0?<Button onClick={()=>{setChosenQnIndex(chosenQnInex-1)}}>Previous</Button>:null
+                        }
+                        {
+                            chosenQnInex<(Questions.length-1)?<Button onClick={()=>{setChosenQnIndex(chosenQnInex+1)}}>Next</Button>:null
+                        }
+                    </div>
 
+                    
+                </div>
+
+                <div className='responsesStatus'>
+
+                    <h4>Responses</h4>
+
+                    <Grid container spacing={{ xs: 1, md: 1 }} columns={{ xs: 2, sm: 2, md: 2 }}>
+                        {
+                            questionNumbers.map(questionNumber => 
+                            <Grid item xs={3} sm={3} md={3} key={questionNumber}>
+                                <div className='questionBox'>
+                                    {questionNumber}
+                                </div>
+                                    
+                            </Grid>)
+                        }
+                    </Grid>
+                    
+                </div>
+            </div>
         </>
       
   )
