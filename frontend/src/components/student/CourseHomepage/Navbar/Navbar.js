8cc71f4876e8addec83de94ceda60d433fe87912
Amit <amitmakkad18@gmail.com>
2022-04-12 19:15:32 +0530

    updated student profile

--- frontend/src/components/student/CourseHomepage/Navbar/Navbar.js
+++ frontend/src/components/student/CourseHomepage/Navbar/Navbar.js
@@ -27,7 +27,7 @@ function Navbar(course) {
        
         <nav  className="navbar navbar-expand-lg navbar-dark bg-primary">
           <div className="container-fluid">
-              <Button onClick={()=>{navigate('/')}} style={{color:"white"}}>Home</Button>
+              <Button onClick={()=>{navigate('/student')}} style={{color:"white"}}>Home</Button>
 
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
               <span className="navbar-toggler-icon"></span>
